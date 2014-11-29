f02f24e4d28a4aac8dab989543679d6946789837
keephopealive <speros.misirlkis@gmail.com>
2014-11-29 13:25:57 -0800

    Changed TEST
    Testing commits

--- application/views/main.php
+++ application/views/main.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Login/Registration</title>
+	<title>Landing Page</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 	<style type="text/css">
 	.right{
