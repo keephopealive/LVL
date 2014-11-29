4c5c98a45e1011b5589b62ee92ddb172d5637450
David Kim <chosendalk@gmail.com>
2014-11-29 02:32:26 -0800

    DK committed changes
    OrderNew form styling

--- application/views/orderNew.php
+++ application/views/orderNew.php
@@ -19,110 +19,120 @@
 		</div>
 	</div>
 
-<form method='post' action='process.php'>
-	<div class="row">
-		<input type="hidden" name="price" value="F">
-		<div class="col-xs-2">
-			<h3>Plate Orientation</h3>
-			<label>
-				<input type="radio" id="orientation_horizontal" name="plate_orientation" value=""> Horizontal
-			</label>
-			<br>
-			<label>
-				<input type="radio" id="orientation_vertical" name="plate_orientation" value=""> Vertical
-			</label>
-		</div>
-		<div class="col-xs-2" id="plate_size">
-			<h3>Plate Size</h3>
-			<p>Dependent upon Plate Orientation choice</p>
-		</div>
-		<div class="col-xs-2">
-			<h3>Collection</h3>
-			<label>
-				<input type="radio" name="collection" value="C"> Classique
-			</label>
-			<br>
-			<label>
-				<input type="radio" name="collection" value="E"> Elipse
-			</label>
-			<br>
-			<label>
-				<input type="radio" name="collection" value="P"> Pierrot
-			</label>
-			<br>
-			<label>
-				<input type="radio" name="collection" value="L"> Limoges
-			</label>
-			<br>
-			<label>
-				<input type="radio" name="collection" value="K"> Damier
-			</label>
+	<div class="row top50 tool">
+			<div class="col-sm-10 col-sm-offset-1">
+				<form method='post' action='custom' role="form" class="form-inline">
+					<input type="hidden" name="price" value="F">
+					
+					<div class="col-sm-4" id="orientation">
+						<h4>Orientation</h4>
+						<label>
+							<input type="radio" name="plate_orientation" value=""> Horizontal
+						</label>
+						<br>
+						<label>
+							<input type="radio" name="plate_orientation" value=""> Vertical
+						</label>
+					</div>
+
+					<div class="col-sm-4">
+						<h4>Plate Size</h4>
+						<label>
+							<input type="radio" name="plate_size" value="3008"> 82x82
+						</label>
+						<br>
+						<label>
+							<input type="radio" name="plate_size" value="3001" orientation="horizontal"> 117x82
+						</label>
+						<br>
+						<label>
+							<input type="radio" name="plate_size" value="3002" orientation="horizontal"> 144x82
+						</label>
+						<br>
+						<label>
+							<input type="radio" name="plate_size" value="3000" orientation="vertical"> 82x117 
+						</label>
+						<br>
+						<label>
+							<input type="radio" name="plate_size" value="3003" orientation="vertical"> 82x144
+						</label>
+					</div>
+		
+					<div class="col-sm-4" id="collection">
+						<h4>Collection</h4>
+						<label>
+							<input type="radio" name="collection" value="C"> Classique
+						</label>
+						<br>
+						<label>
+							<input type="radio" name="collection" value="E"> Elipse
+						</label>
+						<br>
+						<label>
+							<input type="radio" name="collection" value="P"> Pierrot
+						</label>
+						<br>
+						<label>
+							<input type="radio" name="collection" value="L"> Limoges
+						</label>
+						<br>
+						<label>
+							<input type="radio" name="collection" value="K"> Damier
+						</label>
+					</div>
+					
+					<div class="col-sm-4">
+						<h4>Finish</h4>
+
+							<select name="finish" class="form-control selectwidthauto">
+								<option value="FA">NICKEL BROSSE</option>
+								<option value="FB">NICKEL BRILLANT </option>
+								<option value="FC">MICROBILLE NICKEL </option>
+								<option value="FD">CHROME MAT</option>
+								<option value="FE">CHROME VIF </option>
+								<option value="FF">CANON DE FUSIL ANTHRACITE</option>
+								<option value="FG">CANON DE FUSIL BLEU NUIT</option>
+								<option value="CA">BM CLAIR</option>
+								<option value="CB">BM CLAIR VERNI MAT</option>
+								<option value="CC">BM ALLEMAND</option>
+								<option value="CD">BM FONCE</option>
+								<option value="CE">CHAMPAGNE</option>
+								<option value="CF">DORE PATINE</option>
+								<option value="CG">LAITON POLI VERNI </option>
+								<option value="CH">LAITON POLI SATINE</option>
+								<option value="SA">NICKEL NOIR BRILLANT</option>
+								<option value="SB">NICKEL NOIR MATTE</option>
+								<option value="SC">CHROME MARTELE</option>
+								<option value="SD">CHROME VIBRE </option>
+								<option value="SE">ARGENT PATINE</option>
+								<option value="SF">MICROBILLE CHROME </option>
+								<option value="SG">CUIVRE PATINE</option>
+								<option value="SH">CUIVRE VIEILLI BOUCHONNE </option>
+								<option value="SI">CUIVRE SATINE</option>
+								<option value="SJ">BM FONCE BAREGE BRILLANT</option>
+								<option value="SK">Dorure 24 carats</option>
+								<option value="SL">Microbillé dorure 24 carats</option>
+								<option value="SM">Microbillé CF anthracite</option>
+								<option value="SN">POLI VERNI OR MAT</option>
+							</select>
+						</label>					
+					</div>
+					<div class="col-sm-4">
+						<h4>Mechanisms</h4>
+						<label>
+							<input type="radio" name="mechanism" value="A1100010"> default
+						</label>
+					</div>
+					<div class="col-sm-4">
+						<h4>Edge/Screw</h4>
+						<label>
+							<input type="radio" name="edge_screw" value="X"> default
+						</label>
+						
+						<button type="submit" class="btn btn-default pull-right top50">Submit</button>
+					</div>
+				</form>
+			</div>
 		</div>
 
-		<div class="col-xs-2">
-			<h3>Edge/Screw</h3>
-			<label>
-				<input type="radio" name="edge_screw" value="X" selected> default
-			</label>
-		</div>
-		<div class="col-xs-2">
-			<h3>Mechanisms</h3>
-			<label>
-				<input name='mechanism' type='radio' value="A1100010"> 1 V&V
-				<input name='mechanism' type='radio' value="A1100201"> 2 INV A LINE
-			</label>
-
-		</div>
-		<div class="col-xs-2">
-			<h3>Finish</h3>
-			<label>
-				<select name="finish">
-					<option value="FA">NICKEL BROSSE</option>
-					<option value="FB">NICKEL BRILLANT </option>
-					<option value="FC">MICROBILLE NICKEL </option>
-					<option value="FD">CHROME MAT</option>
-					<option value="FE">CHROME VIF </option>
-					<option value="FF">CANON DE FUSIL ANTHRACITE</option>
-					<option value="FG">CANON DE FUSIL BLEU NUIT</option>
-					<option value="CA">BM CLAIR</option>
-					<option value="CB">BM CLAIR VERNI MAT</option>
-					<option value="CC">BM ALLEMAND</option>
-					<option value="CD">BM FONCE</option>
-					<option value="CE">CHAMPAGNE</option>
-					<option value="CF">DORE PATINE</option>
-					<option value="CG">LAITON POLI VERNI </option>
-					<option value="CH">LAITON POLI SATINE</option>
-					<option value="SA">NICKEL NOIR BRILLANT</option>
-					<option value="SB">NICKEL NOIR MATTE</option>
-					<option value="SC">CHROME MARTELE</option>
-					<option value="SD">CHROME VIBRE </option>
-					<option value="SE">ARGENT PATINE</option>
-					<option value="SF">MICROBILLE CHROME </option>
-					<option value="SG">CUIVRE PATINE</option>
-					<option value="SH">CUIVRE VIEILLI BOUCHONNE </option>
-					<option value="SI">CUIVRE SATINE</option>
-					<option value="SJ">BM FONCE BAREGE BRILLANT</option>
-					<option value="SK">Dorure 24 carats</option>
-					<option value="SL">Microbillé dorure 24 carats</option>
-					<option value="SM">Microbillé CF anthracite</option>
-					<option value="SN">POLI VERNI OR MAT</option>
-				</select>
-			</label>
-		</div>
-	</div>
-	<div class='row'>
-		<div class='col-xs-12'>
-			<center><h1>
-				Preview Img Here...
-			</h1></center>
-		</div>
-	</div>
-	<div class='row'>
-		<div class='col-xs-12'>
-			<input type='submit' value='Submit Order' class='btn btn-primary'>
-		</div>
-	</div>
-</form>
-
-		</div>
 	</div>
