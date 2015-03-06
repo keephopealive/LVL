b709cc41f3b3377b941963ada54231991ce77df1
David Kim <chosendalk@gmail.com>
2015-03-06 10:55:13 -0800

    Config base_url fix
    from test.lvl-usa.com to lvl-usa.com

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= ''; //http://test.lvl-usa.com/
+$config['base_url']	= ''; //http://lvl-usa.com/
 
 /*
 |--------------------------------------------------------------------------
